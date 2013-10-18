737a93a68a4ef39ce7e65ed98cacd4605e5562a3
picodotdev <pico.dev@gmail.com>
2013-10-18 21:02:16 +0200

    No crear Sid en sesión a menos que sea necesario

--- PlugInTapestry/src/main/java/es/com/blogspot/elblogdepicodev/plugintapestry/mixins/Csrf.java
+++ PlugInTapestry/src/main/java/es/com/blogspot/elblogdepicodev/plugintapestry/mixins/Csrf.java
@@ -33,9 +33,9 @@ public class Csrf {
 	 private Component container;
 
 	 void beginRender(MarkupWriter writer) {
-		  buildSid();
-
 		  if (container instanceof EventLink || container instanceof ActionLink) {
+				buildSid();
+
 				Element element = writer.getElement();
 				String href = element.getAttribute("href");
 				String character = (href.indexOf('?') == -1) ? "?" : "&";
@@ -44,8 +44,6 @@ public class Csrf {
 	 }
 
 	 void afterRenderTemplate(MarkupWriter writer) {
-		  buildSid();
-
 		  if (container instanceof BeanEditForm) {
 				Element form = null;
 				for (Node node : writer.getElement().getChildren()) {
@@ -58,6 +56,8 @@ public class Csrf {
 					 }
 				}
 				if (form != null) {
+					 buildSid();
+
 					 Element e = form.element("input", "type", "hidden", "name", "t:sid", "value", sid.getSid());
 					 e.moveToTop(form);
 				}
@@ -65,17 +65,19 @@ public class Csrf {
 	 }
 
 	 void beforeRenderBody(MarkupWriter writer) {
-		  buildSid();
-		  
 		  if (container instanceof Form) {
+				buildSid();
+
 				Element form = (Element) writer.getElement();
 				form.element("input", "type", "hidden", "name", "t:sid", "value", sid.getSid());
 		  } else if (container instanceof BeanEditForm) {
+				buildSid();
+
 				Element form = (Element) writer.getElement();
 				form.element("input", "type", "hidden", "name", "t:sid", "value", sid.getSid());
 		  }
 	 }
-	 
+
 	 private void buildSid() {
 		  if (sid == null) {
 				sid = Sid.newInstance();
